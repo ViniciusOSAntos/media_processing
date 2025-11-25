e0c7591e345f7e523202da0439095e3b28efaaf9
Vinicius Oliveira <Vinicius Oliveira>
2025-11-24 21:26:50 -0300

    get media setup

--- app/api/v1/media/routes.py
+++ app/api/v1/media/routes.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter
 
 routes = APIRouter(prefix="/v1/media", tags=["media"])
 
+# TODO Return All Medias
 @routes.get("/")
-async def get_medias() -> None:
+async def get_medias() -> dict:
     return {"media_status": "online"}
+
+# TODO Return Media By ID
+@routes.get("/{media_id}")
+async def get_media_by_id(media_id: str) -> dict:
+    return {"media": media_id}
