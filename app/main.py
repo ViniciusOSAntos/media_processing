54d2f4d1e2fda634b7ea2765462257375c4c3233
Vinicius Oliveira <Vinicius Oliveira>
2026-01-09 13:46:49 -0300

    Create Table

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.v1.media.routes import routes as media
+from app.api.v1.media.db import Base, engine
 from app.api.healthcheck import setup_healthcheck
 app = FastAPI(
     title="Media Processing API",
@@ -9,5 +10,6 @@ app = FastAPI(
     debug=False,
 )
 
+Base.metadata.create_all(bind=engine)
 app.include_router(media)
 setup_healthcheck(app)
