f91441cdff43f64f88039f5a998c1c8a5cd28ce3
Vinicius Oliveira <Vinicius Oliveira>
2026-01-09 13:44:04 -0300

    v0 Creating Metadata

--- app/api/v1/media/routes.py
+++ app/api/v1/media/routes.py
@@ -1,15 +1,21 @@
 from typing import List
-from fastapi import APIRouter, UploadFile, File, status, Request
-
+from fastapi import APIRouter, Depends, UploadFile, File, status, Request
+from sqlalchemy.orm import Session
 from app.api.v1.media.services import (
     upload_media_service,
     get_media_by_name_service,
-    delete_media_by_name_service
+    delete_media_by_name_service,
+    create_video_metadata
+)
+
+from app.api.v1.media.db import (
+    get_db
 )
 
 from app.api.v1.media.schemas import (
     MediaReturnSchema
 )
+
 routes = APIRouter(prefix="/v1/media", tags=["media"])
 
 @routes.get(
@@ -29,7 +35,13 @@ async def get_media_by_name(media_name: str) -> dict:
 async def create_media(
     request: Request,
     files: List[UploadFile] = File(...),
+    db: Session = Depends(get_db)
 ) -> List[dict]:
+    create_video_metadata(db, {
+        "name": "video_teste.mp4",
+        "codec": "h.264",
+        "frame_rate": 30
+    })
     return await upload_media_service(files)
 
 @routes.delete(
