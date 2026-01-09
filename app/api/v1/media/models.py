6440859c4641c747b9439a7f75333e6b6b01c66b
Vinicius Oliveira <Vinicius Oliveira>
2026-01-09 13:12:36 -0300

    feat: firt video metadata model

---
+++ app/api/v1/media/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime, func
+from app.api.v1.media.db import Base
+from loguru import logger
+
+class User(Base):
+    __tablename__ = "video_metadata"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(1s00), nullable=False)
+    codec = Column(String(50), nullable=False)
+    width = Column(Integer, nullable=False),
+    height = Column(Integer, nullable=False),
+    frame_rate = Column(Integer, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
