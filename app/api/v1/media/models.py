9b0927bf404fe3297803a4619b062c59ca5f82b6
Vinicius Oliveira <Vinicius Oliveira>
2026-01-09 13:44:50 -0300

    feat: v0 Video Metadata

--- app/api/v1/media/models.py
+++ app/api/v1/media/models.py
@@ -2,13 +2,13 @@ from sqlalchemy import Column, Integer, String, DateTime, func
 from app.api.v1.media.db import Base
 from loguru import logger
 
-class User(Base):
+class VideoMetadata(Base):
     __tablename__ = "video_metadata"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(1s00), nullable=False)
+    name = Column(String(100), nullable=False)
     codec = Column(String(50), nullable=False)
-    width = Column(Integer, nullable=False),
-    height = Column(Integer, nullable=False),
+    # width = Column(Integer, nullable=False),
+    # height = Column(Integer, nullable=False),
     frame_rate = Column(Integer, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
