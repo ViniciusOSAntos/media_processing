f0f0d86dd796b098a709f0b0f62fe5cec4a51823
Vinicius Oliveira <Vinicius Oliveira>
2026-01-11 20:20:50 -0300

    feat: Pydantic validation for v0 video metadata

--- app/api/v1/media/schemas.py
+++ app/api/v1/media/schemas.py
@@ -1,7 +1,10 @@
 from typing import Optional
 from datetime import datetime
 from uuid import UUID
+from typing import Literal
+
 from pydantic import BaseModel, Field
+
 
 class MediaSchema(BaseModel):
     name: str = Field(
@@ -34,3 +37,41 @@ class MediaReturnSchema(MediaSchema):
             "example": "",
         }
     )
+
+# name,codec, frame_rate, created_at, updated_at
+class MediaMetadataSchema(BaseModel):
+    name: str = Field(
+        ...,
+        json_schema_extra={
+            "description": "File name",
+            "example": "teste.mp4"
+        }
+    )
+    codec: str = Field(
+        ...,
+        json_schema_extra={
+            "description": "Algoritmo de Encoder de vídeo",
+            "example": "h.264"
+        }
+    )
+    frame_rate: int = Field(
+        ...,
+        json_schema_extra={
+            "description": "Número de quadros por segundo",
+            "example": "60"
+        }
+    )
+    # created_at: datetime = Field(
+    #     ...,
+    #     json_schema_extra={
+    #         "description": "File creation time",
+    #         "example": "2025-10-06T12:04:51.542978Z",
+    #     }
+    # )
+    # updated_at: datetime = Field(
+    #     ...,
+    #     json_schema_extra={
+    #         "description": "File creation time",
+    #         "example": "2025-10-06T12:04:51.542978Z",
+    #     }
+    # )
